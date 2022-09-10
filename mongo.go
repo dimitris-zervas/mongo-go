51d18f46cc6beb897e4680c563a633039902fa8a
Dimitris Zervas <mitsos@Dimitriss-MacBook-Pro.local>
2022-09-10 13:53:04 +0200

    Add the Connect function

--- mongo.go
+++ mongo.go
@@ -1,9 +1,25 @@
 package mongo
 
-func Hello() string {
-	return "Hey hey"
-}
+import (
+	"context"
+	"log"
 
-func Goodbye() string {
-	return "Goodbye for now!"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connect(context context.Context, uri string) (*mongo.Client, error) {
+	client, err := mongo.Connect(context, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	// defer func() {
+	// 	if err = client.Disconnect(context); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+
+	log.Println("Successfully connected to the Database!")
+	return client, err
 }
